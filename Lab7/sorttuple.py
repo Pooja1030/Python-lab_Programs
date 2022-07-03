b9a04c7f551dad9710205f848466459f472c1707
Pooja1030 <95880894+Pooja1030@users.noreply.github.com>
2022-07-03 21:26:43 +0530

    Update sorttuple.py

--- Lab7/sorttuple.py
+++ Lab7/sorttuple.py
@@ -3,12 +3,7 @@
 def count_digits(M):
     for x in M:
         return sum([len(str(x))])
-
-
 li = [(34,7,3,54), (3,7,5,25), (23,66,85,3), (33,65,43,23)]
-
 print("Original list is:", li)
-
 li.sort(key=count_digits)
-
 print("Sorted list is:", li)
